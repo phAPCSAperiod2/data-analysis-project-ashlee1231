610910e59c040af319ff82aec13a9aeb04dbaf2b
ashlee1231 <ashleekirch11@gmail.com>
2026-02-27 03:43:08 +0000

    debug changes

--- src/App.java
+++ src/App.java
@@ -1,25 +1,19 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Main application for the Data Analysis Mini‑Project.
- *
- * TODO:
- *  - Update the path to your dataset file
- *  - Read the CSV file using Scanner
- *  - Parse each row and extract the correct columns
- *  - Construct Data objects from each row
- *  - Store them in an array
- *  - Write methods to analyze the dataset (min, max, average, filters, etc.)
- *  - Print insights and answer your guiding question
  *  - Add Javadoc comments for any methods you create
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         //CSV file path and scanner setup
         File file = new File("pokemon.csv");
         Scanner scan = new Scanner(file);
+        Scanner userScan = new Scanner(System.in);
 
         //Create an array of Data objects to store data
         Pokemon[] pokemonList = new Pokemon[152];
@@ -42,22 +36,27 @@ public class App {
             // Create a new Pokemon object and add to the array
             pokemonList[i] = new Pokemon(name, type1, type2, attack);
         }
+        scan.close();
 
-        // TODO: Call your analysis methods
+        System.out.println("Guiding Question: /n What pokemon are the strongest?");
         // Example:
         // double maxValue = findMaxValue(dataList);
         // double average = computeAverageValue(dataList);
+        Pokemon strongestPokemon = Pokemon.strongestPokemon(pokemonList);
 
+        System.out.println("What type are you interested in? ");
+        String userInput = userScan.nextLine();
+        double averageValue = Pokemon.avgType(userInput, pokemonList);
 
-        // TODO: Print insights
+        userScan.close();
+
         // - Number of rows loaded
-        // - Min, max, average, or any other findings
+        System.out.println("Number of rows loaded: " + pokemonList.length);
+        
         // - Final answer to your guiding question
-
-
-        // OPTIONAL TODO:
-        // Add user interaction:
-        // Ask the user what kind of analysis they want to see
+        System.out.println("The strongest pokemon is " + strongestPokemon.getName());
+        System.out.println(strongestPokemon.toString());
+        System.out.println("The type you investigated is " + userInput + " with an average attack of " + averageValue);
     }
 
 
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,23 +1,16 @@
 /**
  * Represents one row from your dataset.
  *
- * TODO:
- *  - Rename the class to match your dataset (e.g., Pokemon, StateData, CountryStat)
- *  - Add at least 3 private attributes based on your CSV columns
- *  - Override toString() to display the object's data
  *  - Add Javadoc comments for the class and all methods
  */
 public class Pokemon {
 
-    // TODO: Add at least 3 private attributes
-    // Example:
+
     private String name;
     private String type1;
     private String type2;
     private int attack;
 
-
-    // TODO: Create a constructor that takes all attributes as parameters
     Pokemon (String name, String type1, String type2, int attack) {
         this.name = name;
         this.type1 = type1;
@@ -26,39 +19,55 @@ public class Pokemon {
 
     }
 
-
-    // TODO: Add getters for attributes you need
     public String getName() {
         return name;
     }
     public int getAttack() {
         return attack;
     }
+    public String getType1() {
+        return type1;
+    }
+    public String getType2() {
+        return type2;
+    }
 
-    // TODO: Add other data analysis methods
-    public static String strongestType(Pokemon[] pokemonList){
-        double fireAvg = 0;
-        double waterAvg = 0;
-        double grassAvg = 0;
-        double flyAvg = 0;
-        double poisAvg = 0;
-        double bugAvg = 0;
-        double ghostAvg = 0;
-        double darkAvg = 0;
-        double normAvg = 0;
-        double psyAvg = 0;
-        double fightAvg = 0;
-        double iceAvg = 0;
-        double rockAvg = 0;
-        double elecAvg = 0;
-        double dragonAvg = 0;
+    public static Pokemon strongestPokemon(Pokemon[] pokemonList){
+        Pokemon strongestPokemon = pokemonList[0];
+        for (int i = 0; i < pokemonList.length-1; i++) {
+            if (pokemonList[i].getAttack() > pokemonList[i+1].getAttack()) {
+                strongestPokemon = pokemonList[i];
+            }
+            else {
+                strongestPokemon = pokemonList[i];
+            }
+        }
 
-        for (int i = 0; i < pokemonList.length; i++) {
+        return strongestPokemon;
 
         }
+
+    public static double avgType(String userInputType, Pokemon[] pokemonList) {
+        int sum = 0;
+        int numTypes = 0;
+        for (int i = 0; i < pokemonList.length; i++) {
+            if (pokemonList[i].getType1().equals(userInputType) || pokemonList[i].getType2().equals(userInputType)) {
+                sum += pokemonList[i].getAttack();
+                numTypes++;
+            }
+        }
+        double average = (double)sum / numTypes;
+        System.out.println("Amount of " + userInputType + ": " + numTypes);
+        return average;
     }
     
+    public String toString() {
+        if (this.type2.equals("")) {
 
-    // TODO: Override toString() to return a readable representation of your object
+        return "This pokemon is " + name + "/n Types: " + this.getType1() + " and " + this.getType2() + "/n Attack: " + this.getAttack();
+        }
+
+        return "This pokemon is " + name + "/n Type: " + this.getType1() + "/n Attack: " + this.getAttack();
+    }
 
 }
