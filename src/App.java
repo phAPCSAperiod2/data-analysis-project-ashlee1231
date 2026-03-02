defc9f14da6d519dc4ac68fcced8604cd0913d45
ashlee1231 <ashleekirch11@gmail.com>
2026-03-02 18:29:41 +0000

    debug attempt

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
     public static void main(String[] args) throws FileNotFoundException {
 
         //CSV file path and scanner setup
-        File file = new File("pokemon.csv");
+        File file = new File("Data/pokemon.csv");
         Scanner scan = new Scanner(file);
         Scanner userScan = new Scanner(System.in);
 
